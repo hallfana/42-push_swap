9e3e40559587d1a01f433bff353231b3bf5d02ae
hallfana <hallfana@pop-os.tailbdc9dc.ts.net>
2025-02-18 13:12:29 +0100

    normed

--- instruction/utils.c
+++ instruction/utils.c
@@ -113,28 +113,3 @@ void	ps_free_stack(t_list **stack)
 	}
 	free(stack);
 }
-
-long	ft_atol(char *ptr)
-{
-	long long	i;
-	long long	sign;
-	long long	r;
-
-	i = 0;
-	sign = 1;
-	r = 0;
-	while (ptr[i] && ((ptr[i] >= 9 && ptr[i] <= 13) || ptr[i] == ' '))
-		i++;
-	if (ptr[i] && (ptr[i] == '-' || ptr[i] == '+'))
-	{
-		if (ptr[i] == '-')
-			sign *= -1;
-		i++;
-	}
-	while (ptr[i] && (ptr[i] >= '0' && ptr[i] <= '9'))
-	{
-		r = r * 10 + ptr[i] - '0';
-		i++;
-	}
-	return ((long)(r * sign));
-}
--- utils/ft_utils.c
+++ utils/ft_utils.c
@@ -60,3 +60,28 @@ int	ps_get_distance(t_list **stack, int index)
 	}
 	return (distance);
 }
+
+long	ft_atol(char *ptr)
+{
+	long long	i;
+	long long	sign;
+	long long	r;
+
+	i = 0;
+	sign = 1;
+	r = 0;
+	while (ptr[i] && ((ptr[i] >= 9 && ptr[i] <= 13) || ptr[i] == ' '))
+		i++;
+	if (ptr[i] && (ptr[i] == '-' || ptr[i] == '+'))
+	{
+		if (ptr[i] == '-')
+			sign *= -1;
+		i++;
+	}
+	while (ptr[i] && (ptr[i] >= '0' && ptr[i] <= '9'))
+	{
+		r = r * 10 + ptr[i] - '0';
+		i++;
+	}
+	return ((long)(r * sign));
+}
